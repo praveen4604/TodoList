eb3aed871b34fc04d8f8ea69c1cf6c8088160689
praveen4604 <2004sasipraveenkumar@gmail.com>
2025-07-07 09:29:13 +0530

    Update db.js

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDb=async()=>{
     try{
-        mongoose.connect('mongodb+srv://NCET:NCET_123@cluster0.7yb8v.mongodb.net/TodoList?retryWrites=true&w=majority&appName=Cluster0')
+        mongoose.connect('mongodb url')
         
         console.log("DB connected");
 
